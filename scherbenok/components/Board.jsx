c87ccee7846102390f15c129723fe7e7abeb314a
Kirill Scherbenok <suhorukovrd@gmail.com>
2016-07-22 14:45:09 +0300

    Add simple skeleton with List and Form

--- scherbenok/components/Board.jsx
+++ scherbenok/components/Board.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Lists from './Lists/Lists.jsx'
+import NewListForm from './NewListForm/NewListForm'
 
 export default class Board extends React.Component {
+
     render() {
         return (
-            <div>Board!</div>
+          <div>
+            <NewListForm></NewListForm>
+            <Lists></Lists>
+          </div>
         );
     }
 }
---
+++ scherbenok/components/Lists/Lists.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import List from '../List/List';
+
+export default class Lists extends React.Component {
+  render() {
+      return (
+          <List></List>
+      );
+  }
+};
