33835cff428c144fdfcaeb54456da680f47c208d
Kirill Scherbenok <suhorukovrd@gmail.com>
2016-07-22 18:17:23 +0300

    Finished task

--- scherbenok/components/NewListForm/NewListForm.jsx
+++ scherbenok/components/NewListForm/NewListForm.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Form from '../Form/Form'
+import './NewListForm.css'
 
 export default class NewListForm extends React.Component {
   render() {
       return (
-          <div>
+          <div className='new_list_form'>
             <h2>New list form</h2>
             <Form onClick={this.props.onListCreate} />
           </div>
